5f17897d4028606681208cc3e2599234abdd6dc7
Sonora Digital Corp <sonoradigitalcorp@gmail.com>
2026-07-02 15:22:03 -0700

    debug: return hostname info

--- middleware.js
+++ middleware.js
@@ -1,19 +1,8 @@
 export default function middleware(req) {
   const url = new URL(req.url)
-
-  if (url.hostname === "abe.sonoradigitalcorp.com") {
-    url.pathname = "/abe/index.html"
-    return new Response(null, {
-      headers: { "x-middleware-rewrite": url.toString() },
-    })
-  }
-
-  if (url.hostname === "sonoradigitalcorp.com" || url.hostname === "www.sonoradigitalcorp.com") {
-    url.pathname = "/landing/index.html"
-    return new Response(null, {
-      headers: { "x-middleware-rewrite": url.toString() },
-    })
-  }
+  return new Response(`hostname: ${url.hostname}, pathname: ${url.pathname}, href: ${url.href}`, {
+    headers: { "content-type": "text/plain" },
+  })
 }
 
 export const config = {
